37f214f501d5601c0cc8fa6d8d17269a8ebddf06
Carmelo Quintana <carmeloquintanahernandez@hotmail.com>
2016-04-03 01:40:41 +0100

    Adding more unit tests

--- C4rm4x.WebApi.Cache.OutputCache.Test/Attributes/OutputCacheAttributeTest.OnActionExecuted.cs
+++ C4rm4x.WebApi.Cache.OutputCache.Test/Attributes/OutputCacheAttributeTest.OnActionExecuted.cs
@@ -1,9 +1,12 @@
 ﻿#region Using
 
 using C4rm4x.Tools.TestUtilities;
+using C4rm4x.Tools.Utilities;
 using C4rm4x.WebApi.Framework.Cache;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
+using System;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http.Filters;
@@ -39,7 +42,7 @@ namespace C4rm4x.WebApi.Cache.OutputCache.Test
                 var cache = GetCache();
 
                 CreateSubjectUnderTest()
-                    .OnActionExecuted(GetHttpActionExecutedContext(cache, HttpMethod.Delete));
+                    .OnActionExecuted(GetHttpActionExecutedContext(HttpMethod.Delete, cache));
 
                 Mock.Get(cache)
                     .Verify(c => c.Retrieve<byte[]>(It.IsAny<string>()), Times.Never());
@@ -51,7 +54,7 @@ namespace C4rm4x.WebApi.Cache.OutputCache.Test
                 var cache = GetCache();
 
                 CreateSubjectUnderTest()
-                    .OnActionExecuted(GetHttpActionExecutedContext(cache, HttpMethod.Head));
+                    .OnActionExecuted(GetHttpActionExecutedContext(HttpMethod.Head, cache));
 
                 Mock.Get(cache)
                     .Verify(c => c.Retrieve<byte[]>(It.IsAny<string>()), Times.Never());
@@ -63,7 +66,7 @@ namespace C4rm4x.WebApi.Cache.OutputCache.Test
                 var cache = GetCache();
 
                 CreateSubjectUnderTest()
-                    .OnActionExecuted(GetHttpActionExecutedContext(cache, HttpMethod.Options));
+                    .OnActionExecuted(GetHttpActionExecutedContext(HttpMethod.Options, cache));
 
                 Mock.Get(cache)
                     .Verify(c => c.Retrieve<byte[]>(It.IsAny<string>()), Times.Never());
@@ -75,7 +78,7 @@ namespace C4rm4x.WebApi.Cache.OutputCache.Test
                 var cache = GetCache();
 
                 CreateSubjectUnderTest()
-                    .OnActionExecuted(GetHttpActionExecutedContext(cache, HttpMethod.Post));
+                    .OnActionExecuted(GetHttpActionExecutedContext(HttpMethod.Post, cache));
 
                 Mock.Get(cache)
                     .Verify(c => c.Retrieve<byte[]>(It.IsAny<string>()), Times.Never());
@@ -87,7 +90,7 @@ namespace C4rm4x.WebApi.Cache.OutputCache.Test
                 var cache = GetCache();
 
                 CreateSubjectUnderTest()
-                    .OnActionExecuted(GetHttpActionExecutedContext(cache, HttpMethod.Put));
+                    .OnActionExecuted(GetHttpActionExecutedContext(HttpMethod.Put, cache));
 
                 Mock.Get(cache)
                     .Verify(c => c.Retrieve<byte[]>(It.IsAny<string>()), Times.Never());
@@ -99,7 +102,7 @@ namespace C4rm4x.WebApi.Cache.OutputCache.Test
                 var cache = GetCache();
 
                 CreateSubjectUnderTest()
-                    .OnActionExecuted(GetHttpActionExecutedContext(cache, HttpMethod.Trace));
+                    .OnActionExecuted(GetHttpActionExecutedContext(HttpMethod.Trace, cache));
 
                 Mock.Get(cache)
                     .Verify(c => c.Retrieve<byte[]>(It.IsAny<string>()), Times.Never());
@@ -111,22 +114,102 @@ namespace C4rm4x.WebApi.Cache.OutputCache.Test
                 var cache = GetCache();
 
                 CreateSubjectUnderTest()
-                    .OnActionExecuted(GetHttpActionExecutedContext(cache, HttpMethod.Get));
+                    .OnActionExecuted(GetHttpActionExecutedContext(HttpMethod.Get, cache));
 
                 Mock.Get(cache)
                     .Verify(c => c.Retrieve<byte[]>(It.IsAny<string>()), Times.Once());
             }
 
-            private static HttpActionExecutedContext GetHttpActionExecutedContext(
-                ICache cache,
-                HttpResponseMessage response)
+            [TestMethod, UnitTest]
+            public void OnActionExecuted_Does_Not_Use_ICache_To_Store_Content_When_The_Content_Is_Already_Cached()
             {
-                return GetHttpActionExecutedContext(cache, HttpMethod.Get, response);
+                var cache = GetCache();
+
+                Mock.Get(cache)
+                    .Setup(c => c.Retrieve<byte[]>(Key))
+                    .Returns(GetContent().ToArray());
+
+                CreateSubjectUnderTest()
+                    .OnActionExecuted(GetHttpActionExecutedContext(cache));
+
+                Mock.Get(cache)
+                    .Verify(c => c.Store(It.IsAny<string>(), It.IsAny<object>(), It.IsAny<int>()), 
+                    Times.Never());
+            }
+
+            [TestMethod, UnitTest]
+            public void OnActionExecuted_Does_Not_Use_ICache_To_Store_Content_When_The_ActionExecutedContext_Response_Content_Is_Null()
+            {
+                var cache = GetCache();
+
+                CreateSubjectUnderTest()
+                    .OnActionExecuted(GetHttpActionExecutedContext(null, cache));
+
+                Mock.Get(cache)
+                    .Verify(c => c.Store(It.IsAny<string>(), It.IsAny<object>(), It.IsAny<int>()),
+                    Times.Never());
+            }
+
+            [TestMethod, UnitTest]
+            public void OnActionExecuted_Uses_ICache_To_Store_Content_When_The_ActionExecutedContext_Response_Content_Is_Not_Null()
+            {
+                var cache = GetCache();                
+                var content = GetContent().ToArray();
+                var ServerTimeSpan = ObjectMother.Create<int>();
+
+                CreateSubjectUnderTest(serverTimeSpan: ServerTimeSpan)
+                    .OnActionExecuted(GetHttpActionExecutedContext(content, cache));
+
+                Mock.Get(cache)
+                    .Verify(c => c.Store(Key, content, ServerTimeSpan),
+                    Times.Once());
+            }
+
+            [TestMethod, UnitTest]
+            public void OnActionExecuted_Creates_A_HttpResponseMessage_Without_CacheControl_Header_When_This_Has_An_Item_With_Given_Key_But_ClientTimeSpan_Is_Zero()
+            {
+                var actionExecutedContext = GetHttpActionExecutedContext(GetContent().ToArray());
+
+                CreateSubjectUnderTest(clientTimeSpan: 0)
+                    .OnActionExecuted(actionExecutedContext);
+
+                Assert.IsNull(actionExecutedContext.Response.Headers.CacheControl);
+            }
+
+            [TestMethod, UnitTest]
+            public void OnActionExecuted_Creates_A_HttpResponseMessage_With_CacheControl_Header_When_This_Has_An_Item_With_Given_Key_But_ClientTimeSpan_Is_Not_Zero()
+            {
+                var ClientTimeSpan = ObjectMother.Create<int>();
+                var actionExecutedContext = GetHttpActionExecutedContext(GetContent().ToArray());
+
+                CreateSubjectUnderTest(clientTimeSpan: ClientTimeSpan)
+                    .OnActionExecuted(actionExecutedContext);
+
+                Assert.IsNotNull(actionExecutedContext.Response.Headers.CacheControl);
+                Assert.AreEqual(
+                    TimeSpan.FromSeconds(ClientTimeSpan),
+                    actionExecutedContext.Response.Headers.CacheControl.MaxAge);
+            }
+
+            private static HttpActionExecutedContext GetHttpActionExecutedContext(
+                byte[] content, 
+                ICache cache = null)
+            {
+                return GetHttpActionExecutedContext(
+                    cache ?? GetCache(), 
+                    GetHttpResponseMessage(content));
             }
 
             private static HttpActionExecutedContext GetHttpActionExecutedContext(
                 ICache cache,
+                HttpResponseMessage response = null)
+            {
+                return GetHttpActionExecutedContext(HttpMethod.Get, cache, response);
+            }
+
+            private static HttpActionExecutedContext GetHttpActionExecutedContext(
                 HttpMethod method,
+                ICache cache,                
                 HttpResponseMessage response = null)
             {
                 var actionExecutedContext = new HttpActionExecutedContext(
@@ -134,9 +217,20 @@ namespace C4rm4x.WebApi.Cache.OutputCache.Test
 
                 actionExecutedContext.Response = 
                     actionExecutedContext.ActionContext.Response =
-                    response ?? new HttpResponseMessage(HttpStatusCode.OK);
+                    response ?? GetHttpResponseMessage();
 
                 return actionExecutedContext;
+            }
+
+            private static HttpResponseMessage GetHttpResponseMessage(
+                byte[] content = null)
+            {
+                var response = new HttpResponseMessage(HttpStatusCode.OK);
+
+                if (!content.IsNullOrEmpty())
+                    response.Content = new ByteArrayContent(content);
+
+                return response;
             }
         }
     }
