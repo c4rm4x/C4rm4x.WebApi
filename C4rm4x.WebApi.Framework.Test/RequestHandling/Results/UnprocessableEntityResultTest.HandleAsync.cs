da434020b2fda09f1034d2aa1b69953a5aa0bf98
carmelo <carmelo@purusdigital.com>
2017-06-14 14:00:34 +0100

    Adding support to unprocessable entity

---
+++ C4rm4x.WebApi.Framework.Test/RequestHandling/Results/UnprocessableEntityResultTest.HandleAsync.cs
@@ -0,0 +1,49 @@
+﻿#region Using
+
+using C4rm4x.Tools.TestUtilities;
+using C4rm4x.WebApi.Framework.RequestHandling.Results;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+#endregion
+
+namespace C4rm4x.WebApi.Framework.Test.RequestHandling.Results
+{
+    public partial class UnprocessableEntityResultTest
+    {
+        [TestClass]
+        public class UnprocessableEntityResultExecuteAsyncTest
+        {
+            [TestMethod, UnitTest]
+            public void ExecuteAsync_Returns_UnprocessableEntity_Response()
+            {
+                Assert.AreEqual(
+                    (HttpStatusCode)422,
+                    ExecuteAsync().Result.StatusCode);
+            }
+
+            [TestMethod, UnitTest]
+            public void ExecuteAsync_Returns_Content_As_StringContent()
+            {
+                Assert.IsInstanceOfType(
+                    ExecuteAsync().Result.Content,
+                    typeof(StringContent));
+            }
+
+            private static UnprocessableEntityResult CreateSubjectUnderTest(string reason = null)
+            {
+                return new UnprocessableEntityResult(reason ?? ObjectMother.Create<string>());
+            }
+
+            private static Task<HttpResponseMessage> ExecuteAsync(string reason = null)
+            {
+                return CreateSubjectUnderTest(reason)
+                    .ExecuteAsync(It.IsAny<CancellationToken>());
+            }
+        }
+    }
+}
---
+++ C4rm4x.WebApi.Framework/RequestHandling/Results/UnprocessableEntityResult.cs
@@ -0,0 +1,50 @@
+﻿#region Using
+
+using C4rm4x.Tools.Utilities;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+#endregion
+
+namespace C4rm4x.WebApi.Framework.RequestHandling.Results
+{
+    /// <summary>
+    /// Returns the HTTP code 422 to indicate that server is unable to process the contained instructions
+    /// </summary>
+    public class UnprocessableEntityResult
+    {
+        /// <summary>
+        /// Gets the reason why the request failed to be processed
+        /// </summary>
+        public string Reason { get; private set; }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="reason">The reason</param>
+        public UnprocessableEntityResult(string reason)
+        {
+            reason.NotNullOrEmpty(nameof(reason));
+
+            Reason = reason;
+        }
+
+        /// <summary>
+        ///  Creates an System.Net.Http.HttpResponseMessage asynchronously
+        /// </summary>
+        /// <param name="cancellationToken">The cancellation token</param>
+        /// <returns>The task containing the HTTP response with the 422 code</returns>
+        public Task<HttpResponseMessage> ExecuteAsync(
+            CancellationToken cancellationToken)
+        {
+            return Task.FromResult(Execute());
+        }
+
+        private HttpResponseMessage Execute()
+        {
+            return HttpResponseMessageUtils.Create((HttpStatusCode)422, Reason);
+        }
+    }
+}
